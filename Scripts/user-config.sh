7eea5b00716f4ced57e61d11b79aa5acb33f8d04
Ondrej Rysavy <rysavy@fit.vutbr.cz>
2018-12-14 11:38:28 +0100

    Update user-config.sh

--- Scripts/user-config.sh
+++ Scripts/user-config.sh
@@ -12,3 +12,11 @@ dotnet restore /distance/Distance.sln
 
 # Workaround to missing mapping of wlibpcap library in Linux:
 cp /usr/lib64/libpcap.so.1 $HOME/.nuget/packages/sharppcap/4.5.0/lib/netstandard2.0/libwpcap.so
+
+# download sample datasets
+mkdir pcaps
+cd pcaps
+wget www.fit.vutbr.cz/~rysavy/distance.datasets/testbed-16.pcap
+wget www.fit.vutbr.cz/~rysavy/distance.datasets/testbed-32.pcap
+wget www.fit.vutbr.cz/~rysavy/distance.datasets/testbed-64.pcap
+wget www.fit.vutbr.cz/~rysavy/distance.datasets/testbed-128.pcap
