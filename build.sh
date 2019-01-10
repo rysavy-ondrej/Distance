fd5f89a5c7a3531860501578b24bbec4b2f8fa74
Ondrej Rysavy <rysavy@fit.vutbr.cz>
2019-01-10 09:27:39 +0100

    Modify build and run scripts for Vagrant environment

--- build.sh
+++ build.sh
@@ -6,5 +6,9 @@ dotnet build Distance.sln
 # copy output from projects to artifacts folder:
 cp -a DistanceEngine/bin/Debug/netcoreapp2.1/* artifacts
 
+# copy output from projects to artifacts folder:
+cp -a Profiles/Diagnostics.Soho/bin/Debug/netstandard2.0/Diagnostics.Soho.dll artifacts/
+
+
 # Workaround to missing mapping of wlibpcap libary in Linux:
 cp /usr/lib64/libpcap.so.1 ~/.nuget/packages/sharppcap/4.5.0/lib/netstandard2.0/libwpcap.so
